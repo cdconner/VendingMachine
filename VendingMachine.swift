3746f775ebc8ef63860f973746067bd29f175dab
Chris Conner <chris.conner@gmail.com>
2017-08-04 16:09:47 -0500

    First Commit

---
+++ VendingMachine.swift
@@ -0,0 +1,121 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Chris Conner on 7/31/17.
+//  Copyright © 2017 Treehouse Island, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum VendingSelection {
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candybar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+}
+
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws
+    func deposit(_ amount: Double)
+}
+
+struct Item: VendingItem {
+    let price: Double
+    var quantity: Int
+}
+
+enum InventoryError: Error {
+    case invalidResource
+    case conversionFailure
+}
+
+class PlistConverter {
+    static func dictionary(fromFile name: String, ofType type: String) throws -> [String: AnyObject] {
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw InventoryError.invalidResource
+        }
+        
+        guard let dictionary = NSDictionary(contentsOfFile: path) else {
+            throw InventoryError.conversionFailure
+        }
+    }
+}
+
+class FoodVendingMachine: VendingMachine {
+    let selection: [VendingSelection] = [.soda, .dietSoda, .chips, .cookie, .wrap, .sandwich, .candybar, .popTart, .water, .fruitJuice, .sportsDrink, .gum]
+    var inventory: [VendingSelection : VendingItem]
+    var amountDeposited: Double = 10.0
+
+    required init(inventory: [VendingSelection : VendingItem]) {
+        self.inventory = inventory
+    }
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws {
+        
+    }
+    func deposit(_ amount: Double) {
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
